7b1621c1bdd71c4cbc94cf4e0b1b10dc630625e1
L42-AI <lzaitch@gmail.com>
2025-09-24 11:24:45 +0200

    changes

--- A2/luka.py
+++ A2/luka.py
@@ -5,10 +5,12 @@ import random as r
 
 from consts import INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE, POPULATION_SIZE, GENERATIONS
 
-r.seed(42)
-np.random.seed(42)
-
 if __name__ == "__main__":
+
+    seed = 42
+    r.seed(seed)
+    np.random.seed(seed)
+    
     controller_type = NNController
 
     evolution_manager = EvolutionManager(
@@ -19,13 +21,14 @@ if __name__ == "__main__":
     )
     population = evolution_manager.build_population(POPULATION_SIZE)
 
-    fitnesses = evolution_manager.run_baseline(population)
+    fitnesses = evolution_manager.run_baseline(population, seed)
     
     best_weights, logbook = evolution_manager.run_evolution(
         population.copy(),
         generations=GENERATIONS,
         cx_prob=0.5,
-        mut_prob=0.5
+        mut_prob=0.5,
+        exp_name=f"{seed}_experiment_CL_False",
     )
 
     best_weights, logbook = evolution_manager.run_evolution(
@@ -33,5 +36,105 @@ if __name__ == "__main__":
         generations=GENERATIONS,
         cx_prob=0.0,
         mut_prob=1.0,
+        exp_name=f"{seed}_experiment_CL_True",
+        curricular_learning=True
+    )
+
+    seed = 98
+    r.seed(seed)
+    np.random.seed(seed)
+
+    controller_type = NNController
+
+    evolution_manager = EvolutionManager(
+        INPUT_SIZE,
+        HIDDEN_SIZE,
+        OUTPUT_SIZE,
+        controller_type=controller_type
+    )
+    population = evolution_manager.build_population(POPULATION_SIZE)
+
+    fitnesses = evolution_manager.run_baseline(population, seed)
+    
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.5,
+        mut_prob=0.5,
+        exp_name=f"{seed}_experiment_CL_False",
+    )
+
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.0,
+        mut_prob=1.0,
+        exp_name=f"{seed}_experiment_CL_True",
+        curricular_learning=True
+    )
+
+    seed = 123
+    r.seed(seed)
+    np.random.seed(seed)
+    
+    controller_type = NNController
+
+    evolution_manager = EvolutionManager(
+        INPUT_SIZE,
+        HIDDEN_SIZE,
+        OUTPUT_SIZE,
+        controller_type=controller_type
+    )
+    population = evolution_manager.build_population(POPULATION_SIZE)
+
+    fitnesses = evolution_manager.run_baseline(population, seed)
+    
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.5,
+        mut_prob=0.5,
+        exp_name=f"{seed}_experiment_CL_False",
+    )
+
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.0,
+        mut_prob=1.0,
+        exp_name=f"{seed}_experiment_CL_True",
+        curricular_learning=True
+    )
+
+    seed = 98
+    r.seed(seed)
+    np.random.seed(seed)
+
+    controller_type = NNController
+
+    evolution_manager = EvolutionManager(
+        INPUT_SIZE,
+        HIDDEN_SIZE,
+        OUTPUT_SIZE,
+        controller_type=controller_type
+    )
+    population = evolution_manager.build_population(POPULATION_SIZE)
+
+    fitnesses = evolution_manager.run_baseline(population, seed)
+    
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.5,
+        mut_prob=0.5,
+        exp_name=f"{seed}_experiment_CL_False",
+    )
+
+    best_weights, logbook = evolution_manager.run_evolution(
+        population.copy(),
+        generations=GENERATIONS,
+        cx_prob=0.0,
+        mut_prob=1.0,
+        exp_name=f"{seed}_experiment_CL_True",
         curricular_learning=True
     )
