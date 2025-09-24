4054de25d1e64f9dc3dc04592630cb64eda5017b
L42-AI <lzaitch@gmail.com>
2025-09-24 19:09:09 +0200

    fix

--- A2/main.py
+++ A2/main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         controller_type=controller_type,
     )
 
-    best_weights = np.load("results/best_individual_curricular_learning_False.npy")
+    best_weights = np.load("ea_results/42_best_individual_curricular_learning_True.npy")
 
     run.single(
         controller=controller_type(
@@ -30,27 +30,3 @@ if __name__ == "__main__":
         simulation_steps=15_000,
         record_video=True,
     )
-
-    population = evolution_manager.build_population(POPULATION_SIZE)
-
-    best_weights, logbook = evolution_manager.run_evolution(
-        population.copy(),
-        generations=GENERATIONS,
-        cx_prob=0.5,
-        mut_prob=0.5
-    )
-
-    run.single(
-        controller=controller_type(
-            input_size=INPUT_SIZE,
-            hidden_size=HIDDEN_SIZE,
-            output_size=OUTPUT_SIZE,
-            weights=np.array(best_weights),
-        ),
-        simulation_steps=15_000,
-        record_video=True,
-    )
-
-    # best_weights, logbook = evolution_manager.run_evolution_infinite(
-    #     population.copy(), cx_prob=0.5, mut_prob=0.5
-    # )
